8b254d9492d04c2e13d006078fe5cddbdd8fa189
Harsh Patel <harshvpatel1508@gmail.com>
2025-05-03 15:01:27 +0530

    added conditional based url for axios

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
+const API_URL =
+	process.env.NEXT_PUBLIC_MODE === 'development'
+		? 'http://localhost:4000/api'
+		: 'https://subsight-backend.onrender.com/api'
+
 export const axiosInstance = axios.create({
-	baseURL: 'http://localhost:4000/api', 
+	baseURL: API_URL,
 	withCredentials: true,
 	headers: {
 		'Content-Type': 'application/json',
