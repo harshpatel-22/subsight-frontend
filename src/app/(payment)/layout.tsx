f46341e8e3e44186b2e40100c59094050d8cda09
Harsh Patel <harshvpatel1508@gmail.com>
2025-05-13 18:37:43 +0530

    make payment success and fail page protected

---
+++ src/app/(payment)/layout.tsx
@@ -0,0 +1,14 @@
+'use client'
+import ProtectedRoute from '@/components/ProtectedRoute'
+
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<ProtectedRoute>
+			<>{children}</>
+		</ProtectedRoute>
+	)
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
 import ClientLayout from './client-layout'
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
 	title: 'SubSight',
@@ -25,9 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
 		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body>
 				<ClientLayout>{children}</ClientLayout>
 			</body>
 		</html>
